1ecd180993c6e79db597a01e713ae4d7952e7091
nkorres <nkorres@maseurope.com>
2024-05-05 02:26:11 +0300

    retry decorator uses lambda now

--- actions/login.py
+++ actions/login.py
@@ -1,10 +1,9 @@
-from utils.elements.find_element import find_element_wrapper, find_elements_wrapper
+from utils.elements.find_element import find_elements_wrapper
 from utils.elements.wait_for import wait_for
 from utils.general.load_env import app_settings
 from settings.element_selector_dictionary import Elements
 from settings.pages import Pages
 from utils.general.retry_timeout import retry
-from utils.general.init import driverInstance
 
 def login(driver):
     # Wait for the login button to appear on the page
@@ -13,9 +12,9 @@ def login(driver):
     if not is_login_page:
         print("Not in login page")
         return
-    clear_authwall(driver)
     # Login
 
+    clear_authwall(driver)
     email_form = wait_for(driver,Elements.EMAIL)
     email_form.send_keys(app_settings["email"])
 
@@ -29,11 +28,11 @@ def login(driver):
     wait_for(driver=driver,element_name=Elements.PROFILE_BUTTON)
     print("Successfully logged in")
 
-@retry(func_at_fail=driverInstance.refresh)
+@retry(func_at_fail=lambda driver: driver.refresh())
 def clear_authwall(driver):
     current_url=driver.current_url
     paywall_string="authwall"
-
+    
     if paywall_string in current_url:
         driver.get(Pages.LOGIN)
 
