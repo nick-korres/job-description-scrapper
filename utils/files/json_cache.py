351e7d6b01d2c1f8e9bf0262ef0ebb125e8a8aee
nkorres <nkorres@maseurope.com>
2024-05-05 03:59:21 +0300

    json util fixes

--- utils/files/json_cache.py
+++ utils/files/json_cache.py
@@ -2,22 +2,26 @@ import json
 import os
 
 
-def load_json(file: str,) -> dict:
+def load_json(file_path: str,) -> dict:
     try:
-        with open(file, 'r') as f:
+        with open(file_path, 'r') as f:
             return json.load(f)
     except FileNotFoundError:
         return {}
     
-def save_json(file: str, data: dict,merge:bool=False):
+def save_json(file_path: str, data: dict,merge:bool=False):
+    file_exists = os.path.exists(file_path)
     if merge:
-        data_to_save = load_json(file)
+        data_to_save = load_json(file_path)
         data_to_save.update(data)
     else:
-        if os.path.exists(file):
-            os.remove(file)
+        if file_exists:
+            os.remove(file_path)
         data_to_save = data
 
-    with open(file, 'w') as f:
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
+    
+    with open(file_path, 'w') as f:
         json.dump(data_to_save, f)
     return data_to_save
